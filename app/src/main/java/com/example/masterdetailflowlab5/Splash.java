212c745163f612063d821268405b8d40c8ace787
Hassan Khan <44042104+Ha55an-k1@users.noreply.github.com>
2020-10-12 03:05:13 -0400

    Added master detail flow template with splash activity

---
+++ app/src/main/java/com/example/masterdetailflowlab5/Splash.java
@@ -0,0 +1,29 @@
+package com.example.masterdetailflowlab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(Splash.this, MusicianDetailActivity.class));
+
+            }
+        };
+        Timer oppening= new Timer();
+        oppening.schedule(task,5000);
+    }
+}
